adcc53b4861d1eb6d4ac343dc58a8001149f1cd6
VladislavCat <kshift84@gmail.com>
2022-03-02 19:27:43 +0300

    Аргументы[#295521]

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -16,4 +16,21 @@ public class Calculator {
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
     }
+
+}
+
+class ArgMethod {
+
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
+    }
+
+    public static void main(String[] args) {
+        String name = "Job4j";
+        int age = 6;
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+    }
 }
