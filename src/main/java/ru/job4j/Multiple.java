d72d30aa72c65cfe9141b075347168764b7bf189
VladislavCat <kshift84@gmail.com>
2022-03-02 15:21:46 +0300

    Метод multiple, разбор первой программы[#159033]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,9 +1,9 @@
 package ru.job4j;
 
 public class Multiple {
-    public static void main(String[] args){
-        for(int i = 2, x = 1; i < 10; i++){
-            System.out.println(x + " * " + i + " = " + x*i);
+    public static void main(String[] args) {
+        for(int i = 2, x = 1; i < 10; i++) {
+            System.out.println(x + " * " + i + " = " + x * i);
         }
     }
 
