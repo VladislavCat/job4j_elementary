4f7ba01f81fa3f011f91326b1883ba8590fb734a
VladislavCat <kshift84@gmail.com>
2022-03-03 12:56:40 +0300

    Аргументы [#295521 #271131] исправление

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -18,19 +18,3 @@ public class Calculator {
     }
 
 }
-
-class ArgMethod {
-
-    public static void hello(String name, int age) {
-        System.out.println("Hello, " + name + ", age = " + age);
-    }
-
-    public static void main(String[] args) {
-        String name = "Job4j";
-        int age = 6;
-        ArgMethod.hello(name, age);
-        ArgMethod.hello(name, age);
-        ArgMethod.hello(name, age);
-        ArgMethod.hello(name, age);
-    }
-}
