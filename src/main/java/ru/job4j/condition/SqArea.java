424310e98f6c00e643649284c57a05dc6479627b
VladislavCat <kshift84@gmail.com>
2022-03-04 14:12:19 +0300

    Исправление поиска площади прямоуголника

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -1,16 +1,13 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double findH(double p, double k) {
-        return p / (2 * (k + 1));
-    }
-
-    public static double findL(double h, double k) {
-        return h * k;
+    public static double findSquare(double p, double k) {
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        return h * l;
     }
 
     public static void main(String[] args) {
-        System.out.println(" p = 6, k = 2, s = 2, h = " + findH(6, 2)
-                + " l = " + findL(findH(6, 2), 2));
+        System.out.println(" p = 4, k = 1, s = 1, real = " + findSquare(4, 1));
     }
 }
