cde1a8749cb3153f333183cf83da7e77d3fff489
VladislavCat <kshift84@gmail.com>
2022-03-06 11:24:19 +0300

    Исправление Math.abs

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -1,7 +1,5 @@
 package ru.job4j.condition;
 
-import ru.job4j.calculator.Point;
-
 public class ChessBoard {
 
     public static int way(int x1, int y1, int x2, int y2) {
@@ -9,18 +7,12 @@ public class ChessBoard {
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
             if (x1 != x2 && y1 != y2) {
-                rsl = (int) distance(x1, y1, x2, y2);
+                if (Math.abs(x1 - x2) == Math.abs(y1 - y2)) {
+                    return Math.abs(x1 - x2);
+                }
             }
         }
         return rsl;
-    }
-
-    private static double distance(int x1, int y1, int x2, int y2) {
-        if (Math.abs(x1 - x2) == Math.abs(y1 - y2)) {
-            return Math.abs(x1 - x2);
-        } else {
-            return 0;
-        }
     }
 
     private static boolean isValid(int coordinate) {
