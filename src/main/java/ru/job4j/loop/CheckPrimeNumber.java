12e3012cd6d2b772812f1b32c131b1d56bd1715f
VladislavCat <kshift84@gmail.com>
2022-03-06 17:26:27 +0300

    Изменения в поиске простого числа

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,14 +3,13 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int num) {
         boolean res = false;
-        for (int i = 2; i <= num - 1; i++) {
-            if (!(num % i == 0)) {
-                res = true;
-            } else {
-                res = false;
-                break;
-            }
-        }
-        return res;
+        for (int i = 2; i <= num; i++) {
+                if (!(num % i == 0) || i == num) {
+                    res = true;
+                } else {
+                    res = false;
+                    break;
+                }
+            } return res;
     }
 }
