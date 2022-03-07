3763d72085bccb733a881471730acccdda8bd132
VladislavCat <kshift84@gmail.com>
2022-03-07 11:01:38 +0300

    Исправление Matrix

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class Matrix {
     public static int[][] multiple(int size) {
         int[][] matrixMul = new int[size][size];
-        for (int i = 0; i < matrixMul.length; i++) {
-            for (int j = 0; j < matrixMul[i].length; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 matrixMul[i][j] = (i + 1) * (j + 1);
             }
         }
