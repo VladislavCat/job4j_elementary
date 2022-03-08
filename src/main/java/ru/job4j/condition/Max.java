aef6c4d0f353c658f9568212ff6c9e011a31b1f3
VladislavCat <kshift84@gmail.com>
2022-03-08 18:46:22 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -6,7 +6,17 @@ public class Max {
         return left > right ? left : right;
     }
 
+    public static int max(int first, int second, int third) {
+        return max(max(first, second), max(second, third));
+    }
+
+    public static int max(int first, int second, int third, int four) {
+        return max(max(first, second, third), four);
+    }
+
     public static void main(String[] args) {
         System.out.println(max(11, 122));
+        System.out.println(max(11, 122, 1222));
+        System.out.println(max(11, 12, 13, 14));
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -39,4 +39,45 @@ public class MaxTest {
         int expected = 12;
         Assert.assertEquals(result, expected);
     }
+
+    @Test
+    public void whenMax11To100to1000() {
+        int first = 11;
+        int second = 100;
+        int third = 1000;
+        int result = Max.max(first, second, third);
+        int expected = 1000;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax100To100to100() {
+        int first = 100;
+        int second = 100;
+        int third = 100;
+        int result = Max.max(first, second, third);
+        int expected = 100;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax22To10to1() {
+        int first = 22;
+        int second = 10;
+        int third = 1;
+        int result = Max.max(first, second, third);
+        int expected = 22;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax11To100to12to15() {
+        int first = 11;
+        int second = 100;
+        int third = 12;
+        int four = 15;
+        int result = Max.max(first, second, third, four);
+        int expected = 100;
+        Assert.assertEquals(result, expected);
+    }
 }
