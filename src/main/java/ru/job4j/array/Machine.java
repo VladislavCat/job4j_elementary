233da59daeb91966fd717552e0404d3d06ef1312
VladislavCat <kshift84@gmail.com>
2022-03-09 10:31:14 +0300

    Исправление аппарата

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,10 +8,10 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int resSum = money - price;
-        for (int i : coins) {
-            while (i <= resSum) {
-                resSum -= i;
-                rsl[size++] = i;
+        for (int coin : coins) {
+            while (coin <= resSum) {
+                resSum -= coin;
+                rsl[size++] = coin;
             }
         }
         return Arrays.copyOf(rsl, size);
